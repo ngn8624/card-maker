e18b862fcae47ddbc4c8624ece3082fc100eab6f
ngn8624 <ngn8624@gmail.com>
2022-07-28 23:12:06 +0900

    [22-07-28] add AddForm

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -55,7 +55,8 @@ const Maker = ({ authService }) => {
     });
   });
   const addCard = card => {
-    console.log(card);
+    const updated = [...cards, card];
+    setCards(updated);
   };
   return (
     <section className={styles.maker}>
