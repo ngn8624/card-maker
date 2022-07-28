f0c22120c485cd2c3817850685129aa62e454fc0
ngn8624 <ngn8624@gmail.com>
2022-07-28 21:12:43 +0900

    [22-07-28] 화면전환 marker

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.module.css';
 import App from './app';
 import AuthService from './service/auth_service';
 
 const authService = new AuthService();
-ReactDOM.render(
-  <React.StrictMode>
+console.log("index");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
     <App authService={authService} />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // </React.StrictMode>,
 );
+
--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -2,10 +2,23 @@ import firebase from 'firebase';
 import firebaseApp from './firebase';
 
 class AuthService {
+  
   login(providerName) {
+    console.log("AuthService");
     const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    // github 인지 구글인지
     // var provider = new firebase.auth.GithubAuthProvider(); 하기위해서 위 코드 씀
-    return firebaseApp.auth().signInWithPopup(authProvider);
+    return firebaseApp.auth().signInWithPopup(authProvider).catch((error)=> {console.log(error)});
+  }
+
+  logout() {
+    firebase.auth().signOut();
+  }
+
+  onAuthChange(onUserChanged) {
+    firebase.auth().onAuthStateChanged(user => {
+      onUserChanged(user);
+    });
   }
 }
 
